5ff369968637d434c3b8f56a7f8491e09aa26089
Ezzaldin97 <generalezz6@yahoo.com>
2023-05-08 00:04:00 +0300

    update configuration parser script

--- app/config_parser.py
+++ app/config_parser.py
@@ -3,5 +3,5 @@ import os
 
 class Config:
     def __init__(self):
-        with open(os.path.join("conf", "config.yml"), "r") as f:
+        with open(os.path.join(".", "app", "conf", "config.yml"), "r") as f:
             self.config = yaml.safe_load(f)
