82687d2763006d07150c0ca49f88244e0249d030
Ezzaldin97 <generalezz6@yahoo.com>
2023-05-08 18:26:53 +0300

    update routes and prepare running script

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,11 @@
 from flask import Flask, request, jsonify
 import pickle
 import os
-from app.config_parser import Config
 from app.inference import Inference
 from app.validator import Laptop
 from app import app
 import pydantic
 
-conf = Config()
 model = pickle.load(open(os.path.join(".", "app", "bin", "model-pipeline.pkl"), "rb"))
 
 @app.route('/predict', methods=['POST'])
---
+++ run.py
@@ -0,0 +1,7 @@
+from app import app
+from app.config_parser import Config
+
+conf = Config()
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port= conf.config["app_port"], debug=True)
